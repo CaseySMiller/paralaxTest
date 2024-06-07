6a3f0ac86c23f212191ec76efb1b1f70e7ba625c
CaseySMiller <CaseySMiller@yahoo.com>
2024-06-06 22:25:13 -0600

    set up basic handlebars mvc app"

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS parallax_db;
+CREATE DATABASE parallax_db;
---
+++ public/assets/scripts/parallax.js
@@ -0,0 +1,6 @@
+// const Parallax = require('parallax-js')
+
+var scene = document.getElementById('scene');
+var parallaxInstance = new Parallax(scene);
+
+parallaxInstance.friction(0.2, 0.2);
